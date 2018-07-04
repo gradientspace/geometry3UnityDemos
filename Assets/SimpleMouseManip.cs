6d0269ba6b05b25c91faea3c9bac0abc44f5cb70
Ryan Schmidt <rms@rms80.com>
2018-07-04 16:34:05 -0400

    updated to current g3sharp, improved reduce demo

---
+++ Assets/SimpleMouseManip.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace g3
+{
+
+    public class SimpleMouseManip : MonoBehaviour
+    {
+        public GameObject TargetObject;
+
+        public float ScrollSpeed = 1.0f;
+        public float RotateSpeed = 1.0f;
+        public float PanSpeed = 0.1f;
+
+        Vector3f last_mouse_pos;
+        Quaternionf initial_rotation;
+
+        Camera mainCamera;
+        public void Start()
+        {
+            mainCamera = Camera.main;
+            last_mouse_pos = Input.mousePosition;
+            initial_rotation = TargetObject.transform.rotation;
+        }
+
+        public void Update()
+        {
+            Vector3f delta = (Vector3f)Input.mousePosition - last_mouse_pos;
+            Transform x = mainCamera.transform;
+
+            if ( Input.mouseScrollDelta.y != 0 ) {
+                if (mainCamera.orthographic) {
+                    mainCamera.orthographicSize = MathUtil.Clamp(
+                        mainCamera.orthographicSize - ScrollSpeed * Input.mouseScrollDelta.y, 1, 1000);
+                }
+            } else if ( Input.GetMouseButton(1) || Input.GetMouseButton(2) ) {
+                mainCamera.transform.position += 
+                    (-PanSpeed * delta.x * x.right) +
+                    (-PanSpeed * delta.y * x.up);
+
+            } else if ( Input.GetMouseButton(0) ) {
+                Quaternionf rotatelr = Quaternionf.AxisAngleD(x.up, -RotateSpeed * delta.x);
+                Quaternionf rotateud = Quaternionf.AxisAngleD(x.right, RotateSpeed * delta.y);
+                Quaternionf cur_rotation = TargetObject.transform.rotation;
+                Quaternion new_rotation = rotatelr * rotateud * cur_rotation;
+                TargetObject.transform.rotation = new_rotation;
+            }
+
+
+            last_mouse_pos = Input.mousePosition;
+        }
+
+    }
+
+}
--- Assets/reduce_demo/ReduceDemo.cs
+++ Assets/reduce_demo/ReduceDemo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using g3;
 using System.IO;
+using System;
 
 public class ReduceDemo : MonoBehaviour
 {
@@ -13,51 +14,98 @@ public class ReduceDemo : MonoBehaviour
 
     GameObject meshGO;
 
+
+    public int ReduceToCount = 500;
+    public int StepsPerFrame = 100;
+    public bool Loop = true;
+
+    public bool LoadSampleMesh = false;
+    public bool FlipLeftRight = false;
+    public string SampleFileName = "bunny_solid.obj";
+
+
+    static byte[] FloatToByte(float[] array)
+    {
+        var byteArray = new byte[array.Length * sizeof(float)];
+        Buffer.BlockCopy(array, 0, byteArray, 0, byteArray.Length);
+        return byteArray;
+    }
+
 	// Use this for initialization
 	void Start () {
+        
+        meshGO = GameObject.Find("sample_mesh");
+        Mesh unityMesh = meshGO.GetComponent<MeshFilter>().mesh;
+        startMesh = g3UnityUtils.UnityMeshToDMesh(unityMesh);
+        double height = startMesh.CachedBounds.Height;
+
         // find path to sample file
-        string curPath = Application.dataPath;
-        string filePath = Path.Combine(curPath, Path.Combine("..\\sample_files", "bunny_solid.obj"));
+        if (LoadSampleMesh) {
+            string curPath = Application.dataPath;
+            string filePath = Path.Combine(curPath, Path.Combine("..\\sample_files", SampleFileName));
 
-        // load sample file, convert to unity coordinate system, translate and scale to origin
-        startMesh = StandardMeshReader.ReadMesh(filePath);
-        if (startMesh == null)
-            startMesh = new Sphere3Generator_NormalizedCube().Generate().MakeDMesh();
-        MeshTransforms.FlipLeftRightCoordSystems(startMesh);
-        MeshTransforms.Translate(startMesh, -startMesh.CachedBounds.Center);
-        MeshTransforms.Scale(startMesh, 8.0 / startMesh.CachedBounds.MaxDim);
+            // load sample file, convert to unity coordinate system, translate and scale to origin
+            startMesh = StandardMeshReader.ReadMesh(filePath);
+            if (startMesh == null)
+                startMesh = new Sphere3Generator_NormalizedCube().Generate().MakeDMesh();
 
-        // load wireframe shader
-        Material wireframeShader = g3UnityUtils.SafeLoadMaterial("wireframe_shader/Wireframe");
+            if (FlipLeftRight)
+                MeshTransforms.FlipLeftRightCoordSystems(startMesh);
+            MeshTransforms.Scale(startMesh, height / startMesh.CachedBounds.Height);
+            MeshTransforms.Translate(startMesh, -startMesh.CachedBounds.Center);
+            MeshNormals.QuickCompute(startMesh);
+            g3UnityUtils.SetGOMesh(meshGO, startMesh);
+        }
 
-        // create initial mesh
-        meshGO = g3UnityUtils.CreateMeshGO("start_mesh", startMesh, wireframeShader);
     }
-	
 
-	// Update is called once per frame
-	void Update () {
-        if ( Input.GetKeyUp(KeyCode.R) ) {
-            if (active_reduce != null)
+
+    // Update is called once per frame
+    bool in_loop = false;
+    void Update () {
+        in_loop = Loop;
+
+        if (Input.GetKeyUp(KeyCode.L))
+            Loop = !Loop;
+
+        if (Input.GetKeyUp(KeyCode.S)) {
+            if (active_reduce != null) {
                 StopCoroutine(active_reduce);
+                active_reduce = null;
+            }
+        }
+
+        if ( Input.GetKeyUp(KeyCode.R) ) {
+            if (active_reduce != null) {
+                StopCoroutine(active_reduce);
+                active_reduce = null;
+            }
 
             curMesh = new DMesh3(startMesh);
             reduce = new InteractiveReducer(curMesh);
             active_reduce = StartCoroutine(reduce_playback());
         }
-	}
+
+        // if we are looping, restart
+        if (in_loop && active_reduce == null) {
+            curMesh = new DMesh3(startMesh);
+            reduce = new InteractiveReducer(curMesh);
+            active_reduce = StartCoroutine(reduce_playback());
+        }
+    }
 
 
     IEnumerator reduce_playback()
     {
         int iter = 0;
-        int N = 100;
-        foreach (int i in reduce.ReduceToTriangleCount_Interactive(500) ) {
-            if (iter++ % N == 0) {
+        foreach (int i in reduce.ReduceToTriangleCount_Interactive(ReduceToCount) ) {
+            if (iter++ % StepsPerFrame == 0) {
                 g3UnityUtils.SetGOMesh(meshGO, curMesh);
                 yield return new WaitForSecondsRealtime(0.001f);
             }
         }
+        yield return new WaitForSecondsRealtime(1.0f);
+        active_reduce = null;
     }
 
 }
